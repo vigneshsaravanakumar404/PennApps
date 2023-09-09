5c2ada4f511439ab8f1750f7550f4528292c094c
vigneshsaravanakumar404 <10021913@sbstudents.ir>
2023-09-09 09:50:09 -0400

    Landing Page Backend
    Signed-off-by: vigneshsaravanakumar404 <10021913@sbstudents.ir>

---
+++ landingPageBackend.py
@@ -0,0 +1,3 @@
+# Backend Variables
+HEADING = "The Ultimate Collegate Financial Management System"
+SUB_HEADING = "Manage Student Loan Information, expenses, budgeting, learn about financial literacy, and more!"
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Imports
-from signUpBackend import FILE_NAME, HEADING, SUB_HEADING, hash_password, user_exists, save_user, get_users
+from signUpBackend import FILE_NAME, hash_password, user_exists, save_user, get_users
 from signIn import check_password, authenticate_user
+from landingPageBackend import HEADING, SUB_HEADING
 import os
 import json
 
